070e26ef5ea6cc09335a395408a663374f48e13e
tvin4411 <tvin4411@users.github.com>
2019-11-05 14:01:33 +1100

    Initial Commit
    Landing page - has menu button, one image, perks, starting the pyramid but no footer
    Sign up page - has all the HTML and CSS but no JS, icons and footer
    End point page - nothing yet
    All are connected with internal links

---
+++ JS.js
@@ -0,0 +1,87 @@
+// Menu Icon 
+function myMenu(x) {
+    x.classList.toggle("extend");
+}
+
+// Perk Icon Informartion
+function EliteIcon(x) {
+    var x = document.getElementById("speech-pointer1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("speechR1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function LowIcon(x) {
+    var x = document.getElementById("speech-pointer2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("speechR2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function WifiIcon(x) {
+    var x = document.getElementById("speech-pointer3");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("speechR3");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function PointsIcon(x) {
+    var x = document.getElementById("speech-pointer4");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("speechR4");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function AppIcon(x) {
+    var x = document.getElementById("speech-pointer5");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("speechR5");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+// Form Validation
+function validateForm() {
+    
+}
+
+// Local Storage
