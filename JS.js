0164d7367b6477abc425bec58b680db4df47daf9
tvin4411 <tvin4411@users.github.com>
2019-11-07 22:58:00 +1100

    Finished up find a button

--- JS.js
+++ JS.js
@@ -1,9 +1,29 @@
-// Menu Icon
+// Menu Icon will expand showing navigation links to other pages
 function myMenu(x) {
     x.classList.toggle("extend");
 }
 
-// Perk Icon Informartion
+// Find a Hotel Pop-up will appear by clicking the button
+function findbtn(x) {
+    var x = document.getElementById("hotel-opt");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+// Closes Find a Hotel Pop-up and returns to landing page
+function exit(x) {
+    var x = document.getElementById("hotel-opt");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+// Perk Icons opens up a block text of information beside it
 function EliteIcon(x) {
     var x = document.getElementById("speechR1");
     if (x.style.display === "none") {
@@ -49,7 +69,12 @@ function AppIcon(x) {
     }
 }
 
-// Form Validation
+// Menu Icon
+function myDTeir(x) {
+    x.classList.toggle("extend");
+}
+
+// Form Validation ensures users input is on the right track
 function validateForm() {
 
 }
